f1a914889acd9a8f25004efc8a3e4f2a8289db99
Dasha Listopadova <dashalistopadova@gmail.com>
2025-03-19 21:59:32 +0000

    add more eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import pluginReact from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginJs from '@eslint/js';
@@ -12,6 +13,7 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat['jsx-runtime'],
+  reactHooks.configs['recommended-latest'],
   eslintConfigPrettier,
   {
     ignores: ['node_modules/', 'dist/', 'plugins', '.netlify', '.vinxi'],
@@ -34,13 +36,83 @@ export default [
   },
   {
     rules: {
+      '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+      '@typescript-eslint/consistent-type-definitions': 'error',
+      '@typescript-eslint/member-ordering': 'error',
       '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+      '@typescript-eslint/prefer-for-of': 'error',
+      '@typescript-eslint/prefer-function-type': 'error',
+      '@typescript-eslint/unified-signatures': 'error',
       '@typescript-eslint/no-unused-vars': [
         'error',
         { argsIgnorePattern: '^_', ignoreRestSiblings: true },
       ],
+      '@typescript-eslint/no-shadow': 'error',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: ['variable', 'parameter'],
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+        },
+        // для констант
+        {
+          selector: 'variable',
+          modifiers: ['const'],
+          format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+          leadingUnderscore: 'forbid',
+        },
+        // для react компонентов
+        {
+          selector: 'variable',
+          modifiers: ['const'],
+          types: ['function'],
+          format: ['camelCase', 'PascalCase'],
+          leadingUnderscore: 'forbid',
+        },
+        // для react компонентов в качестве параметра
+        {
+          selector: 'parameter',
+          types: ['function'],
+          format: ['camelCase', 'PascalCase'],
+        },
+        {
+          selector: 'function',
+          format: ['camelCase', 'PascalCase'],
+        },
+      ],
+      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
       'arrow-body-style': ['error', 'as-needed'],
+      curly: 'error',
+      'dot-notation': 'error',
+      eqeqeq: ['error', 'smart'],
+      'guard-for-in': 'error',
+      'no-bitwise': 'error',
+      'no-caller': 'error',
       'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'warn',
+      'no-eval': 'error',
+      'no-extra-bind': 'error',
+      'no-invalid-this': 'error',
+      'no-new-func': 'error',
+      'no-new-wrappers': 'error',
+      'no-shadow': 'off',
+      'no-template-curly-in-string': 'error',
+      'no-throw-literal': 'error',
+      'no-undef-init': 'error',
+      'no-underscore-dangle': 'error',
+      'no-unused-expressions': 'error',
+      'no-unused-vars': 'off',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+      'one-var': ['error', 'never'],
+      'prefer-const': 'error',
+      'prefer-object-spread': 'error',
+      'no-param-reassign': ['error'],
+      radix: 'error',
+      'import/no-cycle': 'error',
+      'import/no-duplicates': 'error',
     },
   },
 ];
